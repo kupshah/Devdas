c7dedf19128964b2c87cc0ac42dd9d5368dfc3dc
DESKTOP-8THUP70\Kunaal <kupshah@indiana.edu>
2017-12-19 20:08:57 -0500

    @ everyone reflects isa board members

--- devdas.py
+++ devdas.py
@@ -11,11 +11,11 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
-    # member_ids = ['236636135', '236636136', '236636137', '236636138', '236636139', '236636140', '236636141',
-    #               '236636142', '236636143', '236636144', '236636145', '236636146', '236636147', '236636148',
-    #               '236636149', '236636150', '236636151', '236636152', '236636153', '236636154', '236636155',
-    #               '236636156', '277319712', '277319713', '277319714', '277319715', '277319716', '277319717',
-    #               '277319718', '277319719']
+    member_ids = ['236636135', '236636136', '236636137', '236636138', '236636139', '236636140', '236636141',
+                  '236636142', '236636143', '236636144', '236636145', '236636146', '236636147', '236636148',
+                  '236636149', '236636150', '236636151', '236636152', '236636153', '236636154', '236636155',
+                  '236636156', '277319712', '277319713', '277319714', '277319715', '277319716', '277319717',
+                  '277319718', '277319719']
 
     members = ['Suhas Narendrula', 'Anusha Bhaskarla', 'Sachin Seetharam', 'Karthik Eluri', 'Jagdeep Singh Bhinder', 'Keshav Muralitharan', 'Medha Lingawar', 'Kunaal Shah', 'Minal Patel', 'Jasmine Patel', 'Nivi the Mafi', 'Rahee Patel', 'Tulsi Patel', 'Shreya Kaza', 'Devika Davda', 'Neehar Sachdeva', 'Sid Chawla', 'Penny Bola', 'Shakshi Sambwani', 'Kiran Rajan', 'Amani Karim', 'Gazal Kathuria', 'Mahak Jain', 'Varun Alse', 'Nishant Medicharla', 'Venu Bangla', 'Vedant Chawla', 'Paloma Bhugra', 'Tesh Patel', 'Dhvani Shah']
 
@@ -27,7 +27,7 @@ def webhook():
         send_message("Hello " + data["name"] +"!")
 
     elif "@everyone" in data["text"]:
-        mention_everyone()
+        mention_everyone(members,member_ids)
 
     elif data["text"] == "!random person" or data["text"] == "!random_person":
         pick_random_person(members)
@@ -49,13 +49,11 @@ def send_message(msg):
     json = urlopen(req).read().decode()
 
 
-def mention_everyone(members):
+def mention_everyone(members,ids):
     url = 'https://api.groupme.com/v3/bots/post'
-    vkid = ["2248954", "25561506"]
-    vk = {"Vasu Sheel", "Kunaal Shah"}
 
     string = ""
-    for item in vk:
+    for item in members:
         string +="@" +item + " "
 
     data = {
@@ -63,7 +61,7 @@ def mention_everyone(members):
         'attatchments': {
             'loci': [[0,11], [12,11]],
             'type':'mentions',
-            'user_ids': vkid,
+            'user_ids': ids,
         },
         'text' : string
     }
